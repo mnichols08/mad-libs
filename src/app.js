88ee8bfb5603e3a300513a12e297a3d9576bb341
Mikey Nichols <mnix@journeytocode.io>
2023-03-26 23:29:45 -0400

    initializes app

--- src/app.js
+++ src/app.js
@@ -1,2 +1,42 @@
-import './styles/app.scss';
-document.querySelector("button").addEventListener("click",(()=>alert("webpack is installed")));
+import "./styles/app.scss";
+import stories from './scripts/stories.json' assert {type: 'json'};
+const anchor = document.querySelector("main");
+const startButton = document.querySelector("#init button");
+const createEle = (ele, content, root) => {
+  let container = document.createElement(ele);
+  container.innerHTML = content;
+  root.append(container);
+};
+const init = () => (anchor.innerHTML = "");
+
+const choose = () => {
+  let stories = [{ title: "test" }, { title: "titts" }];
+  // stories can be passed in from the JSON file
+  const section = document.createElement("section");
+  const heading = document.createElement("h2");
+  const ul = document.createElement("ul");
+  stories.map((story) =>
+    createEle(
+      "li",
+      `
+            <div>
+              <img
+                src="https://placehold.jp/150x150.png"
+                alt="placeholder description"
+              />
+              <a href="input.html"><h3>${story.title}</h3></a>
+            </div>
+
+    `,
+      ul
+    )
+  );
+  init();
+  section.classList.add("game");
+  heading.innerText = "Please select one of the stories below to play:";
+  section.prepend(heading);
+  section.append(ul);
+  anchor.append(section);
+  //ul.classList.add("flex wrap no-list center");
+};
+startButton.onclick = choose;
