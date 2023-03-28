aa7d90f21df9f262b40c71aa891e091c77d60f11
Mikey Nichols <mnix@journeytocode.io>
2023-03-28 15:29:36 -0400

    defines function to render initialized app on screen

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-import './styles/app.scss';
-document.querySelector("button").addEventListener("click",(()=>alert("webpack is installed")));
+// declares anchor container app to render within
+const anchor = document.querySelector("main");
+// function to clear the app on demand
+const clear = () => (anchor.innerHTML = "");
+// function to initialize the app on demand
+const init = () => {
+  clear();
+  anchor.innerHTML = `
+  <section id="init">
+        <h2>Welcome</h2>
+        <p>Fill in the blanks and be the funniest person in the Room!</p>
+        <button>&gt; GO MAD</button>
+  </section>
+  `;
+};
+
+// initializes the app on body page load.
+document.body.onload = init;
