712690b52d2e16cadfc94aa8d3bb7b488ccac10f
Mikey Nichols <mnix@journeytocode.io>
2023-04-01 20:16:49 -0400

    fixes some bugs created from json file

--- src/views/renderStoryPage.js
+++ src/views/renderStoryPage.js
@@ -1,38 +1,20 @@
 import clear, { anchor } from "./clear";
 import createEle from "../utils/createEle";
 // function to render a story page
-const renderStoryPage = (scenario, values, title, index) => {
-  const [
-    place,
-    noun,
-    secondNoun,
-    rhymesWithSecondNoun,
-    typeOfBuilding,
-    adjective,
-    appendage,
-    typeOfFood,
-  ] = values;
-  const section = document.createElement("section");
-  createEle("h2", title, section);
-  switch (index) {
-    case 0:
-      scenario
-        .replaceAll("{{place}}", place)
-        .replaceAll("{{noun}}", noun)
-        .replaceAll("{{secondNoun}}", secondNoun)
-        .replaceAll("{{rhymesWithSecondNoun}}", rhymesWithSecondNoun)
-        .replaceAll("{{typeOfBuilding}}", typeOfBuilding)
-        .replaceAll("{{adjective}}", adjective)
-        .replaceAll("{{appendage}}", appendage)
-        .replaceAll("{{typeOfFood}}", typeOfFood)
-        .split("\\n")
-        .map((line) => createEle("p", line, section));
-      clear();
-      anchor.append(section);
-      break;
-    default:
-      console.error(`Sorry, something went wrong.`);
-  }
+const renderStoryPage = (scenario, values, title, index, keys) => {
+  const section = document.createElement("section"); // creates an html element to build page within
+  let madlib = scenario;
+  // loops over the values to render and replaces them with the key index
+  values.forEach((value, i) => {
+    const replaceVar = `{{${keys[i]}}}` // defines this key index using the index of this value
+    madlib = madlib.replaceAll(replaceVar, value) // generates the mad lib by replacing all of the static variables with their passed in values
+  })
+
+  createEle("h2", title, section);   // renders the title
+    madlib.split("\\n") // creates an array from the madlib, creating a new item for each line break
+    .map((line) => createEle("p", line, section)); // maps over the newly created array and created `<p>` tags for each of them, and then appends them inside of the section created initally
+  clear(); 
+  anchor.append(section);
 };
 
 export default renderStoryPage;
