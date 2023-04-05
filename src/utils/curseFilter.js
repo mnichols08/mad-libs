e61881f433d9a149121b2ce66e7d63d914716d06
Mikey Nichols <mnix@journeytocode.io>
2023-04-04 21:43:23 -0400

    adds comments and tidies up code

--- src/utils/curseFilter.js
+++ src/utils/curseFilter.js
@@ -1,25 +1,17 @@
-import badwordsArray from './badwords.json';
+import badwordsArray from './data/badwords.json';
+import funnyPhrases from './data/funnies.json';
 
+// randomly picks a phrase from an array based on the length of itself
+const funnyPhrase = () => funnyPhrases[Math.floor(Math.random() * funnyPhrases.length)]
+
+// a function to take in an array of fields and check if it is PG or not
 const pegarDifferenca = (responses) => {
     let caughtWords;
+    // catches curse words and places them in an array
     if (responses) caughtWords = responses.filter(word => badwordsArray.includes(word))
-    if (caughtWords && caughtWords.length > 0) return false, alert('you is a bad bad bad wittle putty tat')
-    else return true
+    if (caughtWords && caughtWords.length > 0) return false, alert(funnyPhrase()), console.error(funnyPhrase())
+    // checks if we caught any naughty words and if we did we alert the user and console error 
+    else return true 
 }
 
-
 export default pegarDifferenca;
-// buttonSubmit.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   let checkWord = document.querySelectorAll("input");
-//   let valueCheck = [].map.call(checkWord, function (input) {
-//     return input.value;
-//   });
-//   function pegarDiferenca(x) {
-//     let r3 = x.filter((a) => badwordsArray.includes(a));
-//     if (r3 !== "") {
-//       console.log(r3);
-//     }
-//   }
-//   pegarDiferenca(valueCheck);
-// });
